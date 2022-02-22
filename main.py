a3e9180aaf039c776bd442984d5588104d6a6a2d
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-22 01:06:44 +0000

    Calculator window created

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+calculation = ""
+
+
+def add_calculation(symbol):
+    pass
+
+
+def evaluate_calculation():
+    pass
+
+
+def clear_field():
+    pass
+
+root = tk.Tk()
+root.geometry('400x375')
+root.mainloop()
